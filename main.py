fa22a5a0d4446fbeba7c026379b22adfdaaa09bf
Erro13 <96345329+Erro13@users.noreply.github.com>
2021-12-18 16:35:48 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 import requests
+from colorama import Fore,init
+init(autoreset = True)
+asciiart = f"""{Fore.RED}
+  _______    _                _  ___ _ _           
+ |__   __|  | |              | |/ (_) | |          
+    | | ___ | | _____ _ __   | ' / _| | | ___ _ __ 
+    | |/ _ \| |/ / _ \ '_ \  |  < | | | |/ _ \ '__|
+    | | (_) |   <  __/ | | | | . \| | | |  __/ |   
+    |_|\___/|_|\_\___|_| |_| |_|\_\_|_|_|\___|_|                                                                                                   
+"""
 
-api = 'https://discordapp.com/api/v9/invites/'#code invite: eU2mYfJ7JF ;D
+print(asciiart)
+token = input("inserisci il token: ")
+invito = input("inserisci l'inivto: ")
+
 
 headrs = {
-    "authorization" : ''#token
+    "authorization" : token
 }
 
 while True: 
-    requests.post(api, headers=headrs)
+    requests.post(f'https://discordapp.com/api/v9/invites/{invito}', headers=headrs)
